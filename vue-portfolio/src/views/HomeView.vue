6376e55cf669a50230c1a4eb0193565bc8931372
Dave-Njeru <davenjeru9@gmail.com>
2025-12-27 22:24:44 +0300

    Feat: mount footer component and adjust layout
    Features added:
    - mounted footer component
    - adjust layout to grid

--- vue-portfolio/src/views/HomeView.vue
+++ vue-portfolio/src/views/HomeView.vue
@@ -1,11 +1,21 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <div>
     <Navbar />
     <Hero />
+    <Footer />
   </div>
 </template>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
