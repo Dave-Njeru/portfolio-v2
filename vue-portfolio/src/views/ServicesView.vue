c9c3bcdf2d017ef22ca2118ada97231cd1528fd7
Dave-Njeru <davenjeru9@gmail.com>
2026-01-14 14:56:53 +0300

    feat: new view
    - Created a new view for services

---
+++ vue-portfolio/src/views/ServicesView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from "@/components/shared/Navbar.vue"
+import Footer from "@/components/shared/Footer.vue"
+import Main from "@/components/services/Main.vue"
+</script>
+
+<template>
+    <div>
+        <Navbar />
+        <Main />
+        <Footer />
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/styles/base.scss";
+
+div {
+  @include base.container;
+}
+</style>
