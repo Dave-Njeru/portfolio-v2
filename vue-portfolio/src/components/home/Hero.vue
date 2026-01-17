5688f111de68cc188b8b26e42d84a44a8207404a
Dave-Njeru <davenjeru9@gmail.com>
2026-01-17 08:54:05 +0300

    Feat: redesign hero component of home view
    - Rebuilt the component using mobile first approach
    - Added more responsive design

--- vue-portfolio/src/components/home/Hero.vue
+++ vue-portfolio/src/components/home/Hero.vue
@@ -1,122 +1,101 @@
 <template>
   <main>
-    <div id="description">
-      <h1>Dave Ndwiga - IT Specialist</h1>
-      <h2>
-        I am a recent computer science graduate passionate about designing and
-        building applications, collaborating with teams to solve complex
-        problems, and delivering user-friendly solutions that meet clients'
-        needs. I offer services in web development, technical support, and
-        database administration. I seek opportunities to apply my academic
-        training, contribute to innovative projects, and provide valuable
-        expertise to your team.
-      </h2>
-      <div>
-        <button><a href="#">Explore My Projects</a></button>
-        <button><a href="#">Download Resume</a></button>
+    <!-- Hero Section -->
+    <div class="hero-container">
+      <div class="hero-text">
+        <!-- Text Content -->
+        <div class="text-content">
+          <h1>Dave Ndwiga - ICT Specialist</h1>
+          <h2>
+            I am a recent computer science graduate passionate about designing and
+            building applications, collaborating with teams to solve complex
+            problems, and delivering user-friendly solutions that meet clients'
+            needs. I offer services in web development, technical support, and
+            database administration. I seek opportunities to apply my academic
+            training, contribute to innovative projects, and provide valuable
+            expertise to your team.
+          </h2>
+        </div>
+        <!-- CTA Buttons -->
+        <div class="button-group">
+          <button><a href="#">Explore My Projects</a></button>
+          <button><a href="#">Download Resume</a></button>
+        </div>
       </div>
-    </div>
-
-    <div id="hero-image">
-      <img src="@/assets/images/profile.jpg" alt="Hero Image" />
+      <!-- Hero Image -->
+      <div>
+        <img src="@/assets/images/profile.jpg" alt="Hero Image" />
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped lang="scss">
 main {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  padding: 4rem 0;
 
-  div#description {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 2rem;
-    gap: 1.5rem;
-
-    h1 {
-      font: {
-        weight: 900;
-        size: 3.75rem;
-      }
-
-      letter-spacing: -0.05rem;
-      line-height: 1.25;
-      color: #212529;
-    }
-
-    h2 {
-      font: {
-        weight: 400;
-        size: 1.25rem;
-      }
-
-      line-height: 1.5;
-      color: #4b5563;
-    }
-
-    div {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      gap: 1rem;
-
-      button {
-        padding: 0.75rem 1.5rem;
-        font-size: 1rem;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        background-color: #007bff;
-        color: white;
-
-        a {
-          text-decoration: none;
-          color: white;
-        }
-      }
-    }
+  @media (min-width: 640px) {
+    padding: 6rem 0;
   }
 
-  div#hero-image {
-    width: 25rem;
-    height: 25rem;
-    margin: auto;
-
-    img {
-      width: 100%;
-      height: 100%;
-      border-radius: 50%;
-      object-fit: cover;
-    }
+  @media (min-width: 1024px) {
+    padding: 8rem 0;
   }
 }
 
-@media screen and (max-width: 768px) {
-  main {
-    grid-template-columns: 1fr;
-    text-align: center;
+.hero-container {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 3rem;
+  align-items: center;
 
-    div#description {
-      h1 {
-        font-size: 2.25rem;
-      }
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 6rem;
+  }
+}
 
-      h2 {
-        font-size: 1rem;
-      }
+.hero-text {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
 
-      div {
-        flex-direction: column;
-        align-items: center;
-      }
+.text-content {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  text-align: center;
+
+  @media (min-width: 1024px) {
+    text-align: left;
+  }
+
+  h1 {
+    font-size: 2.25rem;
+    font-weight: 900;
+    color: rgb(33, 37, 41);
+    line-height: 1.25;
+    letter-spacing: -0.05em;
+
+    @media (min-width: 640px) {
+      font-size: 3rem;
     }
 
-    div#hero-image {
-      width: 15rem;
-      height: 15rem;
-      margin: 2rem auto 2rem auto;
+    @media (min-width: 1024px) {
+      font-size: 3.75rem;
+    } 
+  }
+
+  h2 {
+    font-size: 1rem;
+    font-weight: 400;
+    color: rgb(75, 85, 99);
+    line-height: 1.5;
+
+    @media (min-width: 640px) {
+      font-size: 1.25rem;
+      line-height: 1.75;
     }
   }
 }
