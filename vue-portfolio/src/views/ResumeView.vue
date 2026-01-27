c46411fdb4ad5b19564b5df27661919e7fa8db91
Dave-Njeru <davenjeru9@gmail.com>
2026-01-27 21:55:35 +0300

    feat: create view
    - Created a view for resume page

---
+++ vue-portfolio/src/views/ResumeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from "@/components/shared/Navbar.vue";
+import Footer from "@/components/shared/Footer.vue";
+import Resume from "@/components/resume/Resume.vue";
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <Resume />
+    <Footer />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/styles/base.scss";
+
+div {
+  @include base.container;
+}
+</style>
