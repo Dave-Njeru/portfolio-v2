8dde909d2d26d7e1f3e42d7c937e41cd4c7a350f
Dave-Njeru <davenjeru9@gmail.com>
2026-01-30 18:55:25 +0300

    feat: add styling to main component of resume view

--- vue-portfolio/src/components/resume/Resume.vue
+++ vue-portfolio/src/components/resume/Resume.vue
@@ -5,7 +5,6 @@ import Experience from './Experience.vue';
 import Education from './Education.vue';
 import Certs from './Certs.vue';
 import Skills from './Skills.vue';
-
 </script>
 
 <template>
@@ -24,3 +23,23 @@ import Skills from './Skills.vue';
         <Skills />
     </main>
 </template>
+
+<style scoped lang="scss">
+main {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    border-radius: 0.75rem;
+    box-shadow: 0 25px 50px -12px rgb(0, 0, 0, 0.25); // Tailwind's shadow-2xl
+    padding: 2rem;
+    background-color: rgb(255, 255, 255); // White background
+
+    @media (min-width: 640px) {
+        padding: 2.5rem;
+    }
+
+    @media (min-width: 768px) {
+        padding: 3rem;
+    }
+}
+</style>
