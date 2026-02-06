1128205ce618f9efafdfa0c44960b12f06c02d35
Dave-Njeru <davenjeru9@gmail.com>
2026-02-06 08:41:29 +0300

    feat: add styling to experience component

--- vue-portfolio/src/components/resume/Experience.vue
+++ vue-portfolio/src/components/resume/Experience.vue
@@ -4,12 +4,12 @@
         <div>
             <!-- Experience Item 1-->
             <div>
-                <div>
+                <div class="experience-header">
                     <h3>Data Management Specialist</h3>
                     <p>Dec 2021 - Present</p>
                 </div>
-                <p>CloudFactory | Remote</p>
-                <ul>
+                <p class="company">CloudFactory | Remote</p>
+                <ul class="job-duties">
                     <li>Processed and annotated datasets with 95% accuracy to support ML projects.
                     </li>
                     <li>Collaborated with cross-functional teams to ensure data quality and consistency.</li>
@@ -19,30 +19,32 @@
             </div>
             <!-- Experience Item 2-->
             <div>
-                <div>
+                <div class="experience-header">
                     <h3>Quality Assurance</h3>
                     <p>Dec 2023 - Aug 2024</p>
                 </div>
-                <p>CloudFactory | Remote</p>
-                <ul>
-                    <li>Collaborated  with Team Leads on project requirements, expectations, and timelines.</li>
-                    <li>Reviewed 15-member team's output for detects, inconsistencies, and improvements achieving 98% defect-free rate.</li>
+                <p class="company">CloudFactory | Remote</p>
+                <ul class="job-duties">
+                    <li>Collaborated with Team Leads on project requirements, expectations, and timelines.</li>
+                    <li>Reviewed 15-member team's output for detects, inconsistencies, and improvements achieving 98%
+                        defect-free rate.</li>
                     <li>Gave constructive feedback and mentorship to resolve challenges.</li>
                     <li>Partnered with QA team to align standards and ensure project consistency.</li>
                 </ul>
             </div>
             <!-- Experience Item 3-->
             <div>
-                <div>
+                <div class="experience-header">
                     <h3>Computer Science Student Attachee</h3>
                     <p>May 2025 - Aug 2025</p>
                 </div>
-                <p>Esoma Solutions Ltd | On-site</p>
-                <ul>
+                <p class="company">Esoma Solutions Ltd | On-site</p>
+                <ul class="job-duties">
                     <li>Assisted in the development and testing of software applications, gaining hands-on
                         experience in real-world projects.
                     </li>
-                    <li>Collaborated with team members to learn best practices in coding, debugging, and project management.
+                    <li>Collaborated with team members to learn best practices in coding, debugging, and project
+                        management.
                     </li>
                     <li>Converted word specs to JSON, boosting development pipeline efficiency.</li>
                 </ul>
@@ -50,3 +52,61 @@
         </div>
     </section>
 </template>
+
+<style scoped lang="scss">
+@use "../../assets/styles/base.scss";
+
+section {
+    h2 {
+        @include base.section-heading;
+    }
+
+    >div {
+        margin-top: 1.5rem;
+    }
+}
+
+.experience-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+
+    h3 {
+        font: {
+            size: 1.125rem;
+            weight: 700;
+        }
+
+        line-height: 1.75rem;
+        color: rgb(33, 33, 33);
+    }
+
+    p {
+        font: {
+            size: 0.875rem;
+            weight: 500;
+        }
+
+        color: #4b5563;
+    }
+}
+
+.company{
+    font: {
+        size: 1rem;
+        weight: 500;
+    }
+
+    line-height: 1.5rem;
+    color: #135bec;
+}
+
+.job-duties {
+    margin: 0.25rem 0 0.25rem 1.25rem;
+    list-style-type: disc;
+    list-style-position: outside;
+    font-size: 1rem;
+    line-height: 1.625;
+    color: #4b5563;
+}
+</style>
