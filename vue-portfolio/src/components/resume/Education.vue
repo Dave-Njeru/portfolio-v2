51fa7346f708e5e5388c925309c8f0dbf4cf537c
Dave-Njeru <davenjeru9@gmail.com>
2026-02-06 11:38:42 +0300

    feat: add styling to education component
    - The mentioned component is under Resume view

--- vue-portfolio/src/components/resume/Education.vue
+++ vue-portfolio/src/components/resume/Education.vue
@@ -4,26 +4,89 @@
         <div>
             <!-- Education Item 1-->
             <div>
-                <div>
+                <div class="education-item">
                     <div>
-                        <span>school</span>
+                        <span class="material-symbols-outlined">school</span>
                         <h3>Bachelor of Science in Computer Science</h3>
                     </div>
                     <p>May 2022 - Dec 2025</p>
                 </div>
-                <p>Mount Kenya University</p>
+                <p class="campus">Mount Kenya University</p>
             </div>
             <!-- Education Item 2-->
             <div>
-                <div>
+                <div class="education-item">
                     <div>
-                        <span>school</span>
+                        <span class="material-symbols-outlined">school</span>
                         <h3>Diploma in Software Engineering</h3>
                     </div>
                     <p>May 2019 - Nov 2021</p>
                 </div>
-                <p>Zetech University</p>
+                <p class="campus">Zetech University</p>
             </div>
         </div>
     </section>
 </template>
+
+<style scoped lang="scss">
+@use "../../assets/styles/base.scss";
+
+section {
+    h2 {
+        @include base.section-heading;
+    }
+
+    >div {
+        margin: 1rem 0;
+    }
+}
+
+.education-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+
+    div {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+
+        span {
+            font-size: 1.25rem;
+            line-height: 1.75rem;
+            color: #4b5563;
+        }
+
+        h3 {
+            font: {
+                size: 1.125rem;
+                weight: 700;
+            }
+
+            line-height: 1.75rem;
+            color: #212121;
+        }
+    }
+
+    p {
+        font: {
+            size: 0.875rem;
+            weight: 500;
+        }
+
+        line-height: 1.25rem;
+        color: #4b5563;
+    }
+}
+
+.campus {
+    font: {
+        size: 1rem;
+        weight: 500;
+    }
+
+    line-height: 1.5rem;
+    padding-left: 1.75rem;
+    color: #135bec;
+}
+</style>
