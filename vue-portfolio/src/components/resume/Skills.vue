0b08c8862f58f30536683f05e30da736edba0f0b
Dave-Njeru <davenjeru9@gmail.com>
2026-02-11 11:42:28 +0300

    feat: add styling to skills section
    - Added styling to the skills section of the resume component

--- vue-portfolio/src/components/resume/Skills.vue
+++ vue-portfolio/src/components/resume/Skills.vue
@@ -1,22 +1,19 @@
 <template>
     <section>
         <h2>Skills</h2>
-        <div>
+        <div class="skills-container">
             <!-- Skill Item 1-->
-            <div>
+            <div class="skills-item">
                 <h4>Frontend</h4>
                 <ul>
                     <li>HTML5</li>
                     <li>CSS3</li>
                     <li>JavaScript (ES6+)</li>
-                    <li>Bootstrap</li>
-                    <li>Tailwind</li>
-                    <li>SCSS</li>
                     <li>Vue.js</li>
                 </ul>
             </div>
             <!-- Skill Item 2-->
-            <div>
+            <div class="skills-item">
                 <h4>Backend</h4>
                 <ul>
                     <li>PHP</li>
@@ -26,8 +23,8 @@
                 </ul>
             </div>
             <!-- Skill Item 3-->
-            <div>
-                <h3>Tools & Technologies</h3>
+            <div class="skills-item">
+                <h4>Tools & Technologies</h4>
                 <ul>
                     <li>Git</li>
                     <li>Linux</li>
@@ -35,8 +32,8 @@
                 </ul>
             </div>
             <!-- Skill Item 4-->
-            <div>
-                <h3>Data Analysis</h3>
+            <div class="skills-item">
+                <h4>Data Analysis</h4>
                 <ul>
                     <li>Excel</li>
                     <li>SQL</li>
@@ -47,3 +44,51 @@
         </div>
     </section>
 </template>
+
+<style scoped lang="scss">
+@use "../../assets/styles/base.scss";
+
+section {
+    h2 {
+        @include base.section-heading;
+    }
+
+    >div {
+        margin-top: 1.5rem;
+    }
+}
+
+.skills-container {
+    display: grid;
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    column-gap: 2rem;
+    row-gap: 1rem;
+
+    @media (min-width: 640px) {
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+
+    @media (min-width: 1024px) {
+        grid-template-columns: repeat(4, minmax(0, 1fr));
+    }
+}
+
+.skills-item {
+    h4 {
+        font: {
+            size: 1rem;
+            weight: 700;
+        }
+
+        line-height: 1.5rem;
+        color: rgb(33, 33, 33);
+    }
+
+    ul {
+        margin: 0.5rem 0 0 1.25rem;
+        list-style-type: disc;
+        list-style-position: outside;
+        color: #4b5563;
+    }
+}
+</style>
