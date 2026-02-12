7b24ce81651782d8661fa0638acd0a7c303201de
Dave-Njeru <davenjeru9@gmail.com>
2026-02-12 23:00:42 +0300

    feat: create main component for the projects view
    - Projects.vue is the main component for the projects view.
    - It will be the parent component for future subcomponents.

---
+++ vue-portfolio/src/components/projects/Projects.vue
@@ -0,0 +1,75 @@
+<template>
+    <main>
+        <!-- PageHeading -->
+        <div>
+            <div>
+                <p>Projects</p>
+                <p>Below are a few projects I have worked on.</p>
+            </div>
+        </div>
+        <!-- Chips -->
+        <div>
+            <button>All</button>
+            <button>Web Development</button>
+            <button>Data Analysis</button>
+        </div>
+        <!-- ImageGrid -->
+        <div>
+            <!-- Project Item 1 -->
+            <div>
+                <div>
+                    <div data-alt="Screenshot of the Health Management System" style="background-image: url('/src/assets/images/health-mgt-system.png');"></div>
+                </div>
+                <div>
+                    <p>Health Management System</p>
+                    <p>This project demonstrates how AI can be integrated into modern web applications through an automated appointment booking system with SMS notifications. It incorporates key features such as AWS cloud storage for scalable data management and two-factor authentication to enhance security. This system was developed as my final year project at MKU University.</p>
+                    <p>JavaScript, PHP, MariaDB, AWS, OpenAI</p>
+                </div>
+            </div>
+            <!-- Project Item 2 -->
+            <div>
+                <div>
+                    <div data-alt="Image mimics a Waste Management System" style="background-image: url('/src/assets/images/waste-mgt-system.jpg');"></div>
+                </div>
+                <div>
+                    <p>Waste Management System</p>
+                    <p>In this project, I served as the backend developer, implementing key admin features, authentication mechanisms, and database architecture and modeling. While the frontend was handled by another developer, I also contributed as a consultant to ensure seamless integration. This role highlighted my expertise in building secure, scalable backend systems for collaborative development environments.</p>
+                    <p>PHP, MariaDB</p>
+                </div>
+            </div>
+            <!-- Project Item 3 -->
+            <div>
+                <div>
+                    <div data-alt="Homepage of the Zeeliu website" style="background-image: url('/src/assets/images/zeeliu.png');"></div>
+                </div>
+                <div>
+                    <p>Zeeliu Designs</p>
+                    <p>This project involved developing a website based on a provided design specification. I was responsible for building the site and ensuring that the content and layout were properly aligned and visually consistent. The website was optimized to function seamlessly across both desktop and mobile views.</p>
+                    <p>HTML, CSS, BootStrap, JavaScript</p>
+                </div>
+            </div>
+            <!-- Project Item 4 -->
+            <div>
+                <div>
+                    <div style="background-image: url('/src/assets/images/web-scraping.jpg');"></div>
+                </div>
+                <div>
+                    <p>Web Scraping using Python</p>
+                    <p>Developed a web scraping solution to automatically extract, clean, and organize data from targeted websites for efficient analysis and reporting.</p>
+                    <p>Python, Google Colab</p>
+                </div>
+            </div>
+            <!-- Project Item 5 -->
+            <div>
+                <div>
+                    <div style="background-image: url('/src/assets/images/data-wrangling.jpg');"></div>
+                </div>
+                <div>
+                    <p>Netflix Data Wrangling</p>
+                    <p>Performed data wrangling on a Netflix dataset to clean, transform, and prepare the data for exploratory analysis and insights generation.</p>
+                    <p>Python, Kaggle</p>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
