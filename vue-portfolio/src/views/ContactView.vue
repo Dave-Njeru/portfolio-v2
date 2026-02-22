d86eabe53d5807464ae3373c79397478dfe334a8
Dave-Njeru <davenjeru9@gmail.com>
2026-02-22 09:06:36 +0300

    feat: create dummy contact component

---
+++ vue-portfolio/src/views/ContactView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Navbar from "@/components/shared/Navbar.vue";
+import Footer from "@/components/shared/Footer.vue";
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <div>
+        <h2>Contact page coming soon!</h2>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/styles/base.scss";
+
+div {
+  @include base.container;
+}
+</style>
