0079a4afc5c6a002c54b9cc03d2e039ffe584d3d
Dave-Njeru <davenjeru9@gmail.com>
2026-04-23 20:27:56 +0300

    refactor: increase SVG icons size

--- vue-portfolio/src/components/contact/Contact.vue
+++ vue-portfolio/src/components/contact/Contact.vue
@@ -261,8 +261,8 @@ button {
 
             svg {
                 display: block;
-                width: 1.25rem;
-                height: 1.25rem;
+                width: 1.5rem;
+                height: 1.5rem;
             }
 
             &:hover {
